966634f65f7c7f4d8b701da71305f32dbc5d6fba
Sebastian Delmont <sd@notso.net>
2012-05-23 11:38:12 -0400

    prevent warning about VERSION already declared

--- lib/dalli/version.rb
+++ lib/dalli/version.rb
@@ -1,3 +1,8 @@
 module Dalli
-  VERSION = '1.0.7'
+  VERSION = '1.0.7.1' unless defined? Dalli::VERSION  
+  # StreetEasy Notes: this file is included twice, 
+  # once in the gemspec as ./lib/dalli/version and once from dalli.rb as dalli/version, 
+  # which causes ruby to warn about "already initialized constant VERSION"
+  # since that's annoying, we added this 'unless defined'
 end
+
